b7e4d3768eba2c17fc19dec365febf475a838d92
tysweezy <tsouza.sf@gmail.com>
2022-12-05 13:58:07 -0800

    reddit post idea

--- cli.py
+++ cli.py
@@ -1,9 +1,18 @@
 import argparse
 from advocator.scraper import scoop_meta
 
+# idea for grabbing top reddit posts based on a topic
+
+
+def topic_reddit(subreddit):
+    # loop through list of topics
+    return 'https://www.reddit.com/r/' + subreddit + '/top.json?sort=top&t=day&limit=5'
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='processes resource data')
     parser.add_argument('-l', '--link', action='store')
+    parser.add_argument('-r', '--reddit', action='store')
     args = vars(parser.parse_args())
 
     if args['link']:
